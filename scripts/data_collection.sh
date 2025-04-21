bcc0ddcb5d48e0c57f6c8a7551a300d1a50b563c
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-04-21 20:15:38 +0300

    Added db operations

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -6,3 +6,6 @@ unzip data/us-traffic-congestions-2016-2022.zip -d data/
 mv data/us-traffic-congestions-2016-2022/us_congestion_2016_2022.csv data/
 rm -rf data/us-traffic-congestions-2016-2022
 rm data/us-traffic-congestions-2016-2022.zip
+
+# Inserting everything to the database  
+python scripts/build_projectdb.py
