ea81bc88dea7726240c35bd276408018ae2c2cb9
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-04-25 09:18:08 +0300

    Fixed scripts

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -6,11 +6,3 @@ unzip data/us-traffic-congestions-2016-2022.zip -d data/
 mv data/us-traffic-congestions-2016-2022/us_congestion_2016_2022.csv data/
 rm -rf data/us-traffic-congestions-2016-2022
 rm data/us-traffic-congestions-2016-2022.zip
-
-# Inserting everything to the database
-python scripts/build_projectdb.py
-
-password=$(head -n 1 secrets/.psql.pass)
-
-# Importing everything to the database
-sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb --username team26 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Inserting everything to the database
+python scripts/build_projectdb.py
+
+password=$(head -n 1 secrets/.psql.pass)
+
+# Importing everything to the database
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb --username team26 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
