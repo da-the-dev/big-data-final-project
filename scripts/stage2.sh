c3c00449355b8826877c42d40a1589a93ff9a807
machnevegor <egorikhelp@gmail.com>
2025-04-25 10:17:15 +0300

    chore: clean up

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -2,4 +2,4 @@
 
 password=$(head -n 1 secrets/.hive.pass)
 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team26 -p $password -f sql/db.hql > output/hive_results.txt 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team26 -p $password -f sql/db.hql >output/hive_results.txt
