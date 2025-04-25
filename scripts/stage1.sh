955a1f0fdd43f503e4967dd1fa45b3691350a985
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-04-25 09:31:20 +0300

    Added fix to move avsc and java files

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -7,3 +7,9 @@ password=$(head -n 1 secrets/.psql.pass)
 
 # Importing everything to the database
 sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb --username team26 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
+
+mv -f *.avsc output/
+mv -f *.java output/
+
+hdfs dfs -mkdir -p project/warehouse/avsc
+hdfs dfs -put output/*.avsc project/warehouse/avsc
