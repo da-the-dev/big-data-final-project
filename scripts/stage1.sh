6ad2ac4e9b291e4351ee5f41c1181cbbef47be55
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-04-28 19:16:23 +0300

    Removed partitioning column from scoop

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -6,7 +6,7 @@ python scripts/build_projectdb.py
 password=$(head -n 1 secrets/.psql.pass)
 
 # Importing everything to the database
-sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb --username team26 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb --username team26 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1 --exclude-columns state
 
 mv -f *.avsc output/
 mv -f *.java output/
