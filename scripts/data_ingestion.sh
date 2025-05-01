14c54b957086d6f5b4ba3fad63e12931fdc537b3
Daniil Abrosimov <abrosimov.software@gmail.com>
2025-05-01 23:39:40 +0300

    sqoop fix

--- scripts/data_ingestion.sh
+++ scripts/data_ingestion.sh
@@ -10,6 +10,7 @@ hdfs dfs -rm -r project/warehouse || true
 # Import data from PostgreSQL to HDFS using Sqoop
 echo "Importing data from PostgreSQL to HDFS using Sqoop..."
 sqoop import-all-tables \
+  -Dorg.apache.sqoop.splitter.allow_text_splitter=true \
   --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb \
   --username team26 \
   --password $password \
--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Step 1: PostgreSQL"
-bash scripts/data_storage.sh
+# echo "Step 1: PostgreSQL"
+# bash scripts/data_storage.sh
 
 echo "Step 2: Sqoop"
 bash scripts/data_ingestion.sh
