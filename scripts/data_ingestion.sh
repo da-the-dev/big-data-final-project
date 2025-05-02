a0b447d0c5fd6a2c71b83050e87d812dfae2ef1c
Daniil Abrosimov <abrosimov.software@gmail.com>
2025-05-02 10:27:04 +0300

    different time argument

--- scripts/data_ingestion.sh
+++ scripts/data_ingestion.sh
@@ -10,6 +10,7 @@ hdfs dfs -rm -r -skipTrash project/warehouse || true
 # Import data from PostgreSQL to HDFS using Sqoop
 echo "Importing data from PostgreSQL to HDFS using Sqoop..."
 sqoop import-all-tables \
+  -Dmapreduce.map.java.opts='-Duser.timezone=UTC' \
   -Dorg.apache.sqoop.splitter.allow_text_splitter=true \
   --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team26_projectdb \
   --username team26 \
@@ -18,8 +19,6 @@ sqoop import-all-tables \
   --compress \
   --as-parquetfile \
   --warehouse-dir=project/warehouse \
-  --m 4 \
-  -Dparquet.int64.timestamp.unit=MILLIS \
-  -Dhive.parquet.timestamp.skip.conversion=true \
+  --m 4
 
 echo "Data ingestion completed successfully!"
