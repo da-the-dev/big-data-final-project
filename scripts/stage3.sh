03b615f58d2cd2d7ed7fde03933e3f9a62f9d98d
Daniil Abrosimov <abrosimov.software@gmail.com>
2025-05-05 18:56:33 +0300

    no idea

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source .venv/bin/activate
-
 echo "Creating train/test sets..."
 
 hdfs dfs -rm -r -skipTrash project/data/train || true 
@@ -13,5 +11,3 @@ spark-submit --master yarn scripts/prepare_data.py
 
 hdfs dfs -cat project/data/train/*.json > data/train.json
 hdfs dfs -cat project/data/test/*.json > data/test.json
-
-echo "Data storage completed successfully!"
