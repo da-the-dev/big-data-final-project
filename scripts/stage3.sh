1259521d22e11409229cc173552556c7922b83f9
Daniil Abrosimov <abrosimov.software@gmail.com>
2025-05-05 19:12:25 +0300

    no idea

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -7,6 +7,7 @@ hdfs dfs -rm -r -skipTrash project/data/test || true
 rm -f data/train.json
 rm -f data/test.json
 
+pyspark --master yarn
 spark-submit --master yarn scripts/prepare_data.py
 
 hdfs dfs -cat project/data/train/*.json > data/train.json
