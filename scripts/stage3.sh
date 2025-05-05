05b27ed230c18bcd7bedd20079e79467b4692e1e
Daniil Abrosimov <abrosimov.software@gmail.com>
2025-05-05 20:50:03 +0300

    reduce memory

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -9,7 +9,11 @@ hdfs dfs -rm -r -skipTrash project/data/test || true
 rm -f data/train.json
 rm -f data/test.json
 
-spark-submit --master yarn scripts/prepare_data.py
+spark-submit \
+  --master yarn \
+  --driver-memory 256m \
+  --executor-memory 256m \
+  scripts/prepare_data.py
 
 hdfs dfs -cat project/data/train/*.json > data/train.json
 hdfs dfs -cat project/data/test/*.json > data/test.json
