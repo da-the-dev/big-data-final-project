49bc7bdd4fdcbfda986ef5621c3df64d05bf7bf0
Daniil Abrosimov <abrosimov.software@gmail.com>
2025-05-05 19:33:37 +0300

    maybe maybe maybe

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export HADOOP_CONF_DIR=$HADOOP_HOME%/etc/hadoop
 
 echo "Creating train/test sets..."
 
@@ -7,7 +8,6 @@ hdfs dfs -rm -r -skipTrash project/data/test || true
 rm -f data/train.json
 rm -f data/test.json
 
-pyspark --master yarn
 spark-submit --master yarn scripts/prepare_data.py
 
 hdfs dfs -cat project/data/train/*.json > data/train.json
